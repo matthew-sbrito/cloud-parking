6486c1e373faa20649ead799c87a7d88318038d5
matthew-sbrito <matheusbr032@gmail.com>
2022-08-04 23:07:43 -0300

    fix: add request body create user

--- src/main/java/com/techsoft/parking/authentication/controller/AuthController.java
+++ src/main/java/com/techsoft/parking/authentication/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.techsoft.parking.authentication.controller;
 
 import com.techsoft.parking.authentication.domain.ApplicationUser;
 import com.techsoft.parking.authentication.dto.form.ApplicationUserCreateDTO;
+import com.techsoft.parking.authentication.dto.response.ApplicationUserDTO;
 import com.techsoft.parking.authentication.service.UserDetailsServiceImpl;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -10,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,11 +29,11 @@ public class AuthController {
 
 	@PostMapping
 	@ApiOperation("Create user")
-	public ResponseEntity<ApplicationUser> create(ApplicationUserCreateDTO applicationUserCreateDTO) {
+	public ResponseEntity<ApplicationUserDTO> create(@RequestBody ApplicationUserCreateDTO applicationUserCreateDTO) {
         ApplicationUser user = userDetailsServiceImpl.saveDto(applicationUserCreateDTO);
 
 //        URI uri = URI.create(String.format("/user/%s", user.getId().toString()));
-        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ApplicationUserDTO(user));
 	}
 
 }
